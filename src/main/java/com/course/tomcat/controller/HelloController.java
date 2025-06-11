4327d0ac7da8f94117a127bd8d9f93277e9bb036
vodori6161 <94844251+vodori6161@users.noreply.github.com>
2025-06-11 15:31:57 +0530

    deliberate error (#2)
    * changes made to controller.java post creation of the production branch
    
    this is the commit after making the new production pranch and selecting the poll thingy in the jnkins configuation
    
    H/5 * * * *
    
    and then i went direct to the controller and made these changes and the commit was made directly to the master branch and idk why this is and will be learning this shortly
    
    basically new endpoints were added as per the udemy tutorial
    
    * removed semicolon to cause build error
    
    i deliberately removed a semicolon to cause an error whilst completing the build process using jenkins
    
    the failure error will be notified on my slack channel

--- src/main/java/com/course/tomcat/controller/HelloController.java
+++ src/main/java/com/course/tomcat/controller/HelloController.java
@@ -21,7 +21,9 @@ public class HelloController {
  
     @GetMapping(value = "/hero")
     String hero(){
-        return "My favorite superhero is Batman";
+
+        return "My favorite superhero is Batman"
+
     }
  
 }
